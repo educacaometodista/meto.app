ed0ece9098f0a8f90c21b8ab204a52a580264aa4
LeuAlmeida <leo@webid.net.br>
2019-10-30 13:43:56 -0300

    Replacing all files into src/ folder

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -8,7 +8,7 @@ jest.mock('expo', () => ({
   AppLoading: 'AppLoading',
 }));
 
-jest.mock('../navigation/AppNavigator', () => 'AppNavigator');
+jest.mock('../src/navigation/AppNavigator', () => 'AppNavigator');
 
 describe('App', () => {
   jest.useFakeTimers();
