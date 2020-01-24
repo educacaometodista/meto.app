802f19ae7021a8003e0a0aab9a6420be35504f2e
LeuAlmeida <leo@webid.net.br>
2020-01-24 15:17:00 -0300

    chore: Improving welcome screen

--- src/pages/Welcome/index.js
+++ src/pages/Welcome/index.js
@@ -1,25 +1,39 @@
 import React from 'react';
 import {
   Container,
+  Background,
+  HeaderWelcome,
+  HeaderLogo,
   WelcomeTitle,
+  WelcomeSubtitle,
+  ButtonGroup,
   AlunoButton,
   AlunoText,
   CandidatoButton,
   CandidatoText,
 } from './styles';
+import { Bold } from '../../styles/global';
 
 function Welcome() {
   return (
     <>
       <Container>
-        <WelcomeTitle>Metodista</WelcomeTitle>
-
-        <AlunoButton>
-          <AlunoText>Sou Aluno</AlunoText>
-        </AlunoButton>
-        <CandidatoButton>
-          <CandidatoText>Sou Candidato</CandidatoText>
-        </CandidatoButton>
+        <Background />
+        <HeaderWelcome>
+          <HeaderLogo />
+          <WelcomeTitle>
+            Metô<Bold>App</Bold>
+          </WelcomeTitle>
+          <WelcomeSubtitle>Lorem ipsum dolor sit amet.</WelcomeSubtitle>
+        </HeaderWelcome>
+        <ButtonGroup>
+          <AlunoButton>
+            <AlunoText>Sou Aluno</AlunoText>
+          </AlunoButton>
+          <CandidatoButton>
+            <CandidatoText>Sou Candidato</CandidatoText>
+          </CandidatoButton>
+        </ButtonGroup>
       </Container>
     </>
   );
