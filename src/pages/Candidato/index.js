2b20c1a0fe908e1801b864f23abcbb346d7919c2
LeuAlmeida <leo@webid.net.br>
2020-01-24 18:04:33 -0300

    chore: Initial rnfc to Candidato screen

---
+++ src/pages/Candidato/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+// import { Container } from './styles';
+
+function Candidato() {
+  return (
+    <SafeAreaView>
+      <Text>Olá Candidato!</Text>
+    </SafeAreaView>
+  );
+}
+
+export default Candidato;
