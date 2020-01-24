e542ba23543972e71e5bfc91dc9f613de3d6046a
LeuAlmeida <leo@webid.net.br>
2020-01-24 14:01:55 -0300

    feat: Improving initial styles of welcome page

---
+++ src/pages/Welcome/styles.js
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+// import { darken } from 'polished';
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: blue;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const WelcomeTitle = styled.Text`
+  font-size: 28px;
+  color: #333;
+`;
+
+export const AlunoButton = styled.TouchableOpacity`
+  width: 80%;
+  max-width: 300px;
+  background: #fff;
+  height: 40px;
+  justify-content: center;
+  align-items: center;
+  margin: 20px 0px;
+  border-radius: 20px;
+`;
+
+export const AlunoText = styled.Text`
+  color: #333;
+`;
+
+export const CandidatoButton = styled.TouchableOpacity``;
+
+export const CandidatoText = styled.Text``;
