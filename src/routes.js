3055b08141e24e5949629f411db6e03b281879f6
LeuAlmeida <leo@webid.net.br>
2020-01-24 12:46:06 -0300

    chore: Improving Header options into routes file

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,41 @@
+import React from 'react';
+
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import { LinearGradient } from 'expo-linear-gradient';
 
 import Welcome from './pages/Welcome';
 
 const Routes = createAppContainer(
-  createStackNavigator({
-    Welcome,
-  })
+  createStackNavigator(
+    {
+      Welcome: {
+        screen: Welcome,
+        navigationOptions: {
+          title: 'Seja bem-vindo!',
+          headerShown: true, // Desabilitar para ocultar na home
+        },
+      },
+    },
+    {
+      defaultNavigationOptions: {
+        headerTintColor: '#FFF',
+        headerBackTitleVisible: false,
+        headerStyle: {
+          backgroundColor: '#33CBD4',
+        },
+        headerBackground: () => (
+          <LinearGradient
+            colors={['#153C77', '#1e4e96', '#153C77']}
+            style={{ flex: 1 }}
+            start={{ x: 0, y: 0 }}
+            end={{ x: 1, y: 0 }}
+          />
+        ),
+        headerTitleAlign: 'center',
+      },
+    }
+  )
 );
 
 export default Routes;
