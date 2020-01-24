6702bd74ed15e7ee0e4d226a01150179fcd5acaf
LeuAlmeida <leo@webid.net.br>
2020-01-24 19:15:10 -0300

    chore: Update 'Candidato' stylesheets

--- src/pages/Candidato/styles.js
+++ src/pages/Candidato/styles.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-import bg from '../../assets/img/background.png';
+import bg from '../../assets/img/background2.png';
 
 export const Container = styled.SafeAreaView`
   height: 100%;
@@ -37,7 +37,7 @@ export const DocumentInput = styled.TextInput.attrs({
   padding: 0px 10px;
   height: 60px;
   margin-top: 10px;
-  border-radius: 8px;
+  border-radius: 30px;
   shadow-color: #000;
   shadow-offset: {width: 0, height: 2};
   shadow-opacity: 0.8;
@@ -45,16 +45,40 @@ export const DocumentInput = styled.TextInput.attrs({
   elevation: 2;
 `;
 
-export const SubscribeContainer = styled.View`
+export const ContinueContainer = styled.View`
   margin-top: 10px;
+  justify-content: center;
+  align-items: center;
   width: 100%;
 `;
 
-export const SubscribeButton = styled.TouchableOpacity`
-  justify-content: flex-end;
-  align-items: flex-end;
+export const ContinueButton = styled.TouchableOpacity`
+  justify-content: center;
+  align-items: center;
+  background: #073a78;
+  height: 40px;
+  width: 100%;
+  max-width: 300px;
+  border-radius: 20px;
 `;
 
+export const ContinueText = styled.Text`
+  font-weight: bold;
+  color: #fff;
+  text-transform: uppercase;
+`;
+
+export const SubscribeContainer = styled.View`
+  margin: 0 auto;
+  width: 100%;
+`;
+export const SubscribeButton = styled.TouchableOpacity`
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 60px;
+`;
 export const SubscribeText = styled.Text`
+  color: #fff;
   font-weight: bold;
 `;
