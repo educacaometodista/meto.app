2f35c0d8a2570598dd13adcd5392005b96fe3b91
LeuAlmeida <leo@webid.net.br>
2020-01-24 19:15:02 -0300

    chore: Update 'Candidato' screen with improvements

--- src/pages/Candidato/index.js
+++ src/pages/Candidato/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
-
+import { StatusBar } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 import {
   Container,
   Background,
+  Student,
   DocumentContainer,
   DocumentTitle,
   DocumentInput,
+  ContinueContainer,
+  ContinueButton,
+  ContinueText,
   SubscribeContainer,
   SubscribeButton,
   SubscribeText,
@@ -13,18 +18,45 @@ import {
 
 function Candidato({ navigation }) {
   return (
-    <Container>
-      <Background />
-      <DocumentContainer>
-        <DocumentTitle>Verifique a sua situação</DocumentTitle>
-        <DocumentInput />
+    <>
+      <StatusBar barStyle="light-content" />
+      <Container>
+        <Background />
+
+        <DocumentContainer>
+          <DocumentTitle>Verifique a sua situação</DocumentTitle>
+          <DocumentInput />
+
+          <ContinueContainer>
+            <ContinueButton>
+              <ContinueText>Continuar</ContinueText>
+            </ContinueButton>
+          </ContinueContainer>
+        </DocumentContainer>
+
         <SubscribeContainer>
           <SubscribeButton>
-            <SubscribeText>Inscreva-se no vestibular</SubscribeText>
+            <LinearGradient
+              colors={['#073a78', '#105ab3', '#073a78']}
+              style={{
+                flex: 1,
+                width: '100%',
+                bottom: 0,
+                right: 0,
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+              start={{ x: 0, y: 0 }}
+              end={{ x: 1, y: 0 }}
+            >
+              <SubscribeText>
+                Novo por aqui? Inscreva-se no vestibular
+              </SubscribeText>
+            </LinearGradient>
           </SubscribeButton>
         </SubscribeContainer>
-      </DocumentContainer>
-    </Container>
+      </Container>
+    </>
   );
 }
 
