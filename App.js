ee6d1c82e891dcbf9c3124e6bfa034f0972a45b1
LeuAlmeida <leo@webid.net.br>
2020-01-24 18:30:03 -0300

    chore: Ignore yellowbox warnings

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import { StatusBar } from 'react-native';
+import { StatusBar, YellowBox } from 'react-native';
 
 import Routes from './src/routes';
+
+YellowBox.ignoreWarnings([
+  'Expected style "shadowRadius: 2"',
+  'Node of type rule not supported as an inline style',
+]);
 
 function App() {
   return (
     <>
-      <StatusBar barStyle="light-content" />
+      <StatusBar barStyle="dark-content" />
       <Routes />
     </>
   );
