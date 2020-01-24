683cb4b24675ee09befa3e6dcb52e02630a7f20c
LeuAlmeida <leo@webid.net.br>
2020-01-24 12:31:28 -0300

    chore: Initial rnfc to welcome page

---
+++ src/pages/Welcome/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+function Welcome() {
+  return <View />;
+}
+
+export default Welcome;
