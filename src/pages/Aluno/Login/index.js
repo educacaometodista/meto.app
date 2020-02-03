9d50edc4c3ef9d9b81e7e1db696fa2419cf1b9c3
LeuAlmeida <leo@webid.net.br>
2020-02-03 18:59:49 -0300

    feat: Initial structure of Login page

---
+++ src/pages/Aluno/Login/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+// import { Container } from './styles';
+
+function Login() {
+  return (
+    <View>
+      <Text>Hello Student!</Text>
+    </View>
+  );
+}
+
+export default Login;
