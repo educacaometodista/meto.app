0d0ddfdb26986c06560f3d301432b1405de0d90d
LeuAlmeida <leo@webid.net.br>
2020-02-27 18:44:54 -0300

    feat(Home): create Home screen

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,22 +1,90 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import {
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Dimensions,
+  Alert,
+} from 'react-native';
+import { Block, Text, theme } from 'galio-framework';
+import api from '../../services/api';
+import { argonTheme } from '../../constants';
 
-import { Card } from 'galio-framework';
-// import { Container } from './styles';
+import Articles from '../../components/news';
+
+const { width } = Dimensions.get('screen');
+const cardWidth = width - theme.SIZES.BASE * 2;
 
 export default function Home() {
+  const [news, setNews] = useState([]);
+  const regexImg = /(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|png)/g;
+  useEffect(() => {
+    async function loadNews() {
+      const response = await api.get('/news');
+      setNews(response.data.items);
+    }
+
+    loadNews();
+  }, []);
+
   return (
-    <Card
-      flex
-      style={{ width: '100%', height: 100, paddingHorizontal: 20 }}
-      // borderlass
-      title="Teste"
-      caption="139"
-      avatar="http://i.pravatar.cc/100?id=skater"
-      imageStyle={{ borderRadius: 30 }}
-      location="Los Angeles, CA"
-      imageBlockStyle={{ padding: 20 / 2 }}
-      image="https://images.unsplash.com/photo-1497802176320-541c8e8de98d?&w=1600&h=900&fit=crop&crop=entropy&q=300"
-    />
+    <>
+      <StatusBar barStyle="dark-content" />
+      <Block flex center>
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <Block flex style={styles.group}>
+            <Text bold size={16} style={styles.title}>
+              Últimas Notícias
+            </Text>
+            <Block flex style={{ marginTop: theme.SIZES.BASE / 2 }}>
+              <ScrollView
+                horizontal
+                pagingEnabled
+                decelerationRate={0}
+                scrollEventThrottle={16}
+                snapToAlignment="center"
+                showsHorizontalScrollIndicator={false}
+                snapToInterval={cardWidth + theme.SIZES.BASE * 0.375}
+                contentContainerStyle={{
+                  paddingHorizontal: theme.SIZES.BASE / 2,
+                }}
+              >
+                {news.slice(0, 5).map(article => (
+                  <Articles
+                    key={article.guid}
+                    articleTitle={article.title}
+                    articleImage={
+                      article.content_encoded._.match(regexImg)
+                        ? String(
+                            article.content_encoded._.match(regexImg).slice(
+                              0,
+                              1
+                            )
+                          )
+                        : 'http://placehold.it/400x400'
+                    }
+                    articleUrl={article.guid}
+                    articleDescription={article.description}
+                    articleDate={article.pubDate}
+                  />
+                ))}
+              </ScrollView>
+            </Block>
+          </Block>
+        </ScrollView>
+      </Block>
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  title: {
+    paddingBottom: theme.SIZES.BASE,
+    paddingHorizontal: theme.SIZES.BASE * 2,
+    marginTop: 22,
+    color: argonTheme.COLORS.HEADER,
+  },
+  group: {
+    paddingTop: theme.SIZES.BASE,
+  },
+});
