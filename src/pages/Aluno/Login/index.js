04f678d007b83b8821f13c835a058ea15a28b0a7
LeuAlmeida <leo@webid.net.br>
2020-02-27 13:43:19 -0300

    feat(login): create Login initial screen

--- src/pages/Aluno/Login/index.js
+++ src/pages/Aluno/Login/index.js
@@ -1,15 +1,53 @@
 import React from 'react';
-import { StatusBar, View, Text } from 'react-native';
+import { StatusBar, View, Text, TouchableOpacity } from 'react-native';
 
-// import { Container } from './styles';
+import { Button, Input, Checkbox } from 'galio-framework';
+import { Container, HeaderLogo } from './styles';
 
-function Login() {
+function Login({ navigation }) {
   return (
     <>
-      <StatusBar barStyle="light-content" />
-      <View>
-        <Text>Hello Student!</Text>
-      </View>
+      <Container>
+        <View style={{ alignItems: 'center', marginTop: '5%' }}>
+          <HeaderLogo />
+        </View>
+        <StatusBar barStyle="light-content" />
+        <Input
+          placeholder="E-mail"
+          right
+          icon="mail"
+          family="antdesign"
+          iconColor="#105ab3"
+          style={{ borderColor: '#105ab3', color: '#105ab3' }}
+          email
+        />
+        <Input
+          placeholder="password"
+          password
+          viewPass
+          style={{ borderColor: '#105ab3', color: '#105ab3' }}
+        />
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'flex-start',
+          }}
+        >
+          <Checkbox color="#105ab3" label="Permanecer conectado" />
+        </View>
+
+        <Button
+          onPress={() => navigation.navigate('Home')}
+          color="#105ab3"
+          style={{ marginVertical: 10 }}
+        >
+          <Text style={{ color: '#FFF' }}>Entrar</Text>
+        </Button>
+        <TouchableOpacity onPress={() => navigation.navigate('Subscribe')}>
+          <Text>Quero me inscrever</Text>
+        </TouchableOpacity>
+      </Container>
     </>
   );
 }
