d5ed316dad86ebbef31a6c1dcb748a68aad86b0b
LeuAlmeida <leo@webid.net.br>
2020-03-09 15:05:50 -0300

    chore(App): improvements

--- App.js
+++ App.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import { StatusBar, YellowBox } from 'react-native';
+// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+// import { NavigationContainer } from '@react-navigation/native';
 
 import Routes from './src/routes';
+
+import Chat from './src/pages/Chat';
+
+// const Tab = createBottomTabNavigator();
 
 YellowBox.ignoreWarnings([
   'Expected style "shadowRadius: 2"',
@@ -13,6 +19,11 @@ function App() {
     <>
       <StatusBar barStyle="dark-content" />
       <Routes />
+      {/* <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="Home" component={Chat} />
+        </Tab.Navigator>
+      </NavigationContainer> */}
     </>
   );
 }
